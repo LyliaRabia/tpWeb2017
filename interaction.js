81b02508f4cc41f5370b6e6de1a24ef32b5f21c8
LyliaRabia <rabialylia@gmail.com>
2018-03-14 19:49:11 +0100

    interraction

--- interaction.js
+++ interaction.js
@@ -8,32 +8,32 @@ function DnD(canvas, interactor) {
     // Developper les 3 fonctions gérant les événements
 
     // Associer les fonctions précédentes aux évènements du canvas.
-    this.debutX = 0;
-    this.debutY = 0;
-    this.finX = 0;
-    this.finY = 0;
+    this.initX = 0;
+    this.initY = 0;
+    this.finalX = 0;
+    this.finalY = 0;
 
 
 
     this.mouseDown = function(evt) {
-        this.debutX = getMousePosition(canvas, evt).x;
-        this.debutY = getMousePosition(canvas, evt).y;
+        this.initX = getMousePosition(canvas, evt).x;
+        this.initY = getMousePosition(canvas, evt).y;
 
-        console.log(this.debutX + " - " + this.debutY);
+        console.log(this.initX + " - " + this.initY);
     }.bind(this);
 
     this.mouseMove = function(evt) {
-        this.finX = getMousePosition(canvas, evt).x;
-        this.finY = getMousePosition(canvas, evt).y;
+        this.finalX = getMousePosition(canvas, evt).x;
+        this.finalY = getMousePosition(canvas, evt).y;
 
         console.log(this.finX + " - " + this.finY);
     }.bind(this);
 
     this.mouseUp = function(evt) {
-        this.finX = getMousePosition(canvas, evt).x;
-        this.finY = getMousePosition(canvas, evt).y;
+        this.finalX = getMousePosition(canvas, evt).x;
+        this.finalY = getMousePosition(canvas, evt).y;
 
-        console.log(this.finX + " - " + this.finY);
+        console.log(this.finalX + " - " + this.finalY);
     }.bind(this);
 
     canvas.addEventListener('mousedown', this.mouseDown, false);
